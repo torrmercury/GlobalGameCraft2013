07814dad1e3b16c63d909461941be868b1fecb77
Lyle Sterne <lms559@nyu.edu>
2013-09-20 16:51:55 -0400

    Finished the finisher script

---
+++ Lost Doorways/Assets/Scripts/Finish.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnTriggerEnter()
+	{
+		Application.LoadLevel ("The Room");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
