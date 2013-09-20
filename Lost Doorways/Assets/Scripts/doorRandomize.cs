64e016fdc4d43d35eb04a3777fcbb8014aa3c732
Matt Chen <matt.chen312@gmail.com>
2013-09-20 12:36:24 -0400

    added room and updated door randomizer

--- Lost Doorways/Assets/Scripts/doorRandomize.cs
+++ Lost Doorways/Assets/Scripts/doorRandomize.cs
@@ -3,20 +3,20 @@ using System.Collections;
 
 public class doorRandomize : MonoBehaviour {
     public Transform testDoor;
-
+    
 	// Use this for initialization
 	void Start () {
-        transform.position += new Vector3(-3f, -3f, -3f);
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+
 	}
 
     void OnBecameInvisible()
     {
-        Instantiate(testDoor, new Vector3(3f, 3f, 3f), Quaternion.identity);
+        Instantiate(testDoor, new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), Random.Range(-2f, 2f)), Quaternion.identity);
         Destroy(gameObject);
     }
 }
