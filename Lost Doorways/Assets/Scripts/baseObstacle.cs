bbdecc4e5acf0953497538996f678e5e2fe753fd
Lyle Sterne <lms559@nyu.edu>
2013-09-20 16:37:50 -0400

    Made pillar prefab and finished the baseObstabcle

--- Lost Doorways/Assets/Scripts/baseObstacle.cs
+++ Lost Doorways/Assets/Scripts/baseObstacle.cs
@@ -2,49 +2,53 @@ using UnityEngine;
 using System.Collections;
 
 public class baseObstacle : MonoBehaviour{
-    bool p1near = true;
+    bool p1near = false;
     bool topped = false;
     float i = 0;
 
 	void start(){
 		
+	}	
+	
+	void OnTriggerEnter() 
+	{
+		p1near = true;
+		Debug.Log("sadiufhkj:");
 	}
-
+	
+	void OnTriggerExit() 
+	{
+		p1near = false;
+		Debug.Log("sadiufhkj:");
+	}
+	
     void Update()
     {
-        if (p1near == true && topped == false)
+		if (p1near == true && topped == false)
         {
-            if ( i < 7)
+            if ( i < 55)
             {
-                transform.position += new Vector3(0, 5, 0) * Time.deltaTime;
+                transform.position += new Vector3(0, 2, 0) * Time.deltaTime;
                 i += 1;
-                Debug.Log(i);
             }
-            else if (i >= 7)
+            else if (i >= 55)
             {
                 topped = true;
-                p1near = false;
-                Debug.Log("hi");
                 i = 0;
-                Debug.Log(i);
             }
         }
         else if (p1near == false && topped == true)
         {
 
-            if (i < 7)
+            if (i < 55)
             {
-                transform.position += new Vector3(0, -5, 0) * Time.deltaTime;
+                transform.position += new Vector3(0, -2, 0) * Time.deltaTime;
                 i += 1;
-                Debug.Log(i);
             }
-            else if (i >= 7)
+            else if (i >= 55)
             {
                 topped = false;
-                p1near = true;
-                Debug.Log("hi");
                 i = 0;
-                Debug.Log(i);
             }
         }
     }
--- Lost Doorways/Assets/Scripts/doorRandomize.cs
+++ Lost Doorways/Assets/Scripts/doorRandomize.cs
@@ -2,45 +2,25 @@
 using System.Collections;
 
 public class doorRandomize : MonoBehaviour {
-    public Transform datDoor;
-    int rng = 0;
-    
+    public Transform[] spawnPoints;
+	public Transform datDoor;
+	
 	// Use this for initialization
 	void Start () {
-        if (datDoor.renderer.isVisible)
-        {
-            Instantiate(datDoor, new Vector3(Random.Range(-2f, 2f), 0f, 2.98f), Quaternion.identity);
-            Destroy(gameObject);
-        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        rng = Random.Range(0, 3);
 	}
 
     void OnBecameInvisible()
     {
-        if (rng == 0)
-        {
-            Instantiate(datDoor, new Vector3(Random.Range(-4.5f, 4.5f), 0f, 8.9f), Quaternion.identity);
-            Destroy(gameObject);
-        }
-        if (rng == 1)
-        {
-            Instantiate(datDoor, new Vector3(Random.Range(-4.5f, 4.5f), 0f, -8.9f), Quaternion.identity);
-            Destroy(gameObject);
-        }
-        if (rng == 2)
-        {
-            Instantiate(datDoor, new Vector3(Random.Range(-2f, 2f), 0f, 2.98f), Quaternion.identity);
-            Destroy(gameObject);
-        }
-        if (rng == 3)
-        {
-            Instantiate(datDoor, new Vector3(Random.Range(-2f, 2f), 0f, 2.98f), Quaternion.identity);
-            Destroy(gameObject);
-        }
+        int numberOfSpawnPoints = spawnPoints.Length;
+		int ranNum = Random.Range(0,numberOfSpawnPoints);
+		transform.parent = spawnPoints[ranNum];
+		transform.localPosition =  Vector3.zero;
+		transform.localRotation = Quaternion.identity;
+		Debug.Log(ranNum);
         
     }
 }
