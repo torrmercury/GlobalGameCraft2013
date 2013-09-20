24987d53d71b385b6c718e5d44969a7600ae2651
Kevin Harper <kevinpaulharper@gmial.com>
2013-09-20 15:29:19 -0400

    DoorMechanic

---
+++ Lost Doorways/Assets/KevinsTests/Scripts/DoorPrefab.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorPrefab : MonoBehaviour {
+	public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.A)){
+			findNewLocation();	
+		}
+	}
+	void OnBecameInvisible(){
+		Debug.Log("I am invisible");
+		findNewLocation();
+		
+	}
+	
+	
+	void findNewLocation(){
+		int numberOfSpawnPoints = spawnPoints.Length;
+		int ranNum = Random.Range(0,numberOfSpawnPoints);
+		transform.parent = spawnPoints[ranNum];
+		transform.localPosition =  Vector3.zero;
+		transform.localRotation = Quaternion.identity;
+		Debug.Log(ranNum);
+		
+	}
+}
